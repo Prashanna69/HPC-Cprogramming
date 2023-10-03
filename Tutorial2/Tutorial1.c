a14c942e04357ba03735441ec2db54259248ab08
Prashanna Lohani <plohani2003@gmail.com>
2023-10-03 13:47:01 +0545

    Week2 Tutorial

---
+++ Tutorial2/Tutorial1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int *m, size;
+  printf("Enter the values of size:");
+  scanf("%d", &size);
+  m = (int *)malloc(size * sizeof(int));
+  printf("Base address of the memory allocated on heap:%d\n", m);
+  printf("Total allocated memory:%d Byte\n", sizeof(int) * size);
+  for (int i = 0; i < size; i++) {
+    printf("%d Array element store in %d Address \n", *(m + i), (m + i));
+  }
+}
